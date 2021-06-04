aaefcbd686389f8b90c22a03f35b02c349f408ad
tonyvx <anthony.valantra@gmail.com>
2021-06-04 17:50:14 -0400

    Updated checked lookup

--- ShoppingList.js
+++ ShoppingList.js
@@ -1,17 +1,13 @@
 import React, { useState } from "react";
-import { FlatList, View } from "react-native";
+import { View } from "react-native";
 import {
-  Searchbar,
   Appbar,
-  List,
-  Divider,
-  Text,
-  Chip,
+  Button,
   Checkbox,
+  Chip,
+  List,
+  Searchbar,
 } from "react-native-paper";
-
-import { IKContext, IKImage, IKUpload } from "imagekit-react";
-import { Hidden } from "@material-ui/core";
 
 export const ShoppingList = ({ list }) => {
   const [selected, setSelected] = useState([]);
@@ -27,55 +23,73 @@ export const ShoppingList = ({ list }) => {
     <View style={{ margin: 16 }}>
       <Appbar.Header>
         <Appbar.Content title="Shopping List" subtitle="Shop away..." />
-        <Searchbar
-        onChangeText={setSearchQuery}
-          value={searchQuery}
-          style={{ width: 200 }}
-        />
       </Appbar.Header>
+      <Searchbar
+        onChangeText={setSearchQuery}
+        value={searchQuery}
+        style={{ width: "80%", alignSelf: "center", margin: 8 }}
+      />
+      <Button onPress={() => setShowSelected(!showSelected)} icon="camera">
+        Show Selected
+      </Button>
       <View style={{ overflow: "auto" }}>
         {list &&
           list
-            .filter((item) =>
-              item.filter(
-                (i) =>
-                  !searchQuery || i.title.toLowerCase().includes(searchQuery.toLowerCase())
-              )
+            .filter(
+              (item) =>
+                itemsToBeDisplayed(item, searchQuery, showSelected, selected)
+                  .length
             )
             .map((item) => (
-              <View
-                key={item[0].shopping_category_id}
-                style={{ width: "80vw" }}
-              >
+              <View key={item[0].shopping_category_id}>
                 <Chip>{item[0].shopping_category_id}</Chip>
-                {item
-                  .filter(
-                    (i) =>
-                      !searchQuery ||
-                      i.title.toLowerCase().includes(searchQuery.toLowerCase())
-                  )
-                  .map((i) => (
-                    <View style={{ display: "flex", flexDirection: "row" }}>
-                      <Checkbox
-                        style={{ margin: 4 }}
-                        onPress={check(i.id)}
-                        status={
-                          selected && selected.includes(i.id)
-                            ? "checked"
-                            : "unchecked"
-                        }
-                      />
-                      <List.Item
-                        key={i.id}
-                        title={i.title}
-                        description={i.notes}
-                        left={(props) => <List.Icon {...props} icon="folder" />}
-                      ></List.Item>
-                    </View>
-                  ))}
+                {itemsToBeDisplayed(
+                  item,
+                  searchQuery,
+                  showSelected,
+                  selected
+                ).map((i) => (
+                  <View
+                    style={{ display: "flex", flexDirection: "row" }}
+                    key={i.id}
+                  >
+                    <Checkbox
+                      style={{ margin: 4 }}
+                      onPress={check(i.id)}
+                      status={
+                        selected && selected.includes(i.id)
+                          ? "checked"
+                          : "unchecked"
+                      }
+                    />
+                    <List.Item
+                      key={i.id}
+                      title={i.title}
+                      description={i.notes}
+                      left={(props) => <List.Icon {...props} icon="folder" />}
+                    ></List.Item>
+                  </View>
+                ))}
               </View>
             ))}
       </View>
     </View>
   );
 };
+function itemsToBeDisplayed(item, searchQuery, showSelected, selected) {
+  return item.filter(
+    (i) =>
+      matchesSearch(searchQuery, i) &&
+      showSelectedItems(showSelected, selected, i)
+  );
+}
+
+function showSelectedItems(showSelected, selected, i) {
+  return !showSelected || selected.includes(i.id);
+}
+
+function matchesSearch(searchQuery, i) {
+  return (
+    !searchQuery || i.title.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+}
